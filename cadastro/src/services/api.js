3742a2d78a06b34503a33b375717511308599c0f
Gabriel Maurício <mauriciorjgabriel@gmail.com>
2024-09-26 17:37:12 -0300

    Update api.js

--- cadastro/src/services/api.js
+++ cadastro/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://cadastro-usuario-red.vercel.app', // Substitua pela URL do back-end
 });
 
 export default api;
